3c53a80c6e826b998d8e1df875ac602541a2c687
TravelersTavern <iamjoenava@gmail.com>
2026-03-09 22:36:32 -0600

    Primer commit

--- app/src/main/java/com/example/myapplication/Inicio.java
+++ app/src/main/java/com/example/myapplication/Inicio.java
@@ -4,13 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Inicio extends AppCompatActivity implements View.OnClickListener {
-    private Button b,b2,b3,b4;
-    private TextView t;
+    private Button b,b2,b3,b4,b5;
+    private EditText e;
+    private TextView t,t8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,23 +21,35 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener {
         b=(Button) findViewById(R.id.button);
         b2=(Button) findViewById(R.id.button2);
         b3=(Button) findViewById(R.id.button3);
-        b4=(Button) findViewById(R.id.button2);
+        b4=(Button) findViewById(R.id.button4);
+        b5=(Button) findViewById(R.id.button5);
+
+        e=(EditText) findViewById(R.id.editTextText);
 
         t=(TextView) findViewById(R.id.textView4);
+        t8=(TextView) findViewById(R.id.textView8);
 
         b.setOnClickListener(this);
         b2.setOnClickListener(this);
         b3.setOnClickListener(this);
         b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
+
         if(view.getId()==R.id.button2) {
-            Intent intent = new Intent(Inicio.this,Vista1.class);
-            startActivity(intent);
-            finish();
+            String texto = e.getText().toString();
+            if(texto.equals("admin")){
+                Intent intent = new Intent(Inicio.this,Vista1.class);
+                startActivity(intent);
+                finish();
+            }else{
+                t8.setText("Usuario incorrecto. Intenta de nuevo.");
+            }
+
         }
         if(view.getId()==R.id.button3){
             Intent intent = new Intent(Inicio.this,Vista2.class);
@@ -47,7 +61,13 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener {
             startActivity(intent);
             finish();
         }
+        if(view.getId()==R.id.button5){
+            String texto = e.getText().toString();
+            t8.setText("Hola" + texto);
+        }
+
         t.setText("Patas de pollo");
     }
 
 }
+
--- app/src/main/java/com/example/myapplication/Vista1.java
+++ app/src/main/java/com/example/myapplication/Vista1.java
@@ -1,16 +1,72 @@
+
 package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Vista1 extends AppCompatActivity {
+public class Vista1 extends AppCompatActivity implements View.OnClickListener{
+
+    private Button bs,br,bm,bd;
+    private EditText eNum1, eNum2;
+    private TextView tResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista1);
+
+        bs=(Button) findViewById(R.id.buttonS);
+        br=(Button) findViewById(R.id.buttonR);
+        bm=(Button) findViewById(R.id.buttonM);
+        bd=(Button) findViewById(R.id.buttonD);
+
+        eNum1=(EditText) findViewById(R.id.editTextNumberDecimal);
+        eNum2=(EditText) findViewById(R.id.editTextNumberDecimal2);
+
+        tResultado=(TextView) findViewById(R.id.textView10);
+
+        bs.setOnClickListener(this);
+        br.setOnClickListener(this);
+        bm.setOnClickListener(this);
+        bd.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        String texto = eNum1.getText().toString();
+        double numero1= Double.parseDouble(texto);
+        String texto2 = eNum2.getText().toString();
+        double numero2= Double.parseDouble(texto2);
+
+        if(view.getId()==R.id.buttonS) {
+            double res =numero1 + numero2;
+            tResultado.setText("Resultado: "+res);
+        }
+        if(view.getId()==R.id.buttonR){
+            double res =numero1 - numero2;
+            tResultado.setText("Resultado: "+res);
+        }
+        if(view.getId()==R.id.buttonM){
+            double res =numero1 * numero2;
+            tResultado.setText("Resultado: "+res);
+        }
+        if(view.getId()==R.id.buttonD){
+            if(numero2==0){
+                tResultado.setText("El numero 2 no puede ser 0");
+            }else{
+                double res =numero1 / numero2;
+                tResultado.setText("Resultado: "+res);
+            }
+
+        }
 
     }
 
