154545de6c3b387e2a52f6b853496a92176b8709
TravelersTavern <iamjoenava@gmail.com>
2026-04-21 14:39:50 -0600

    Primer commit

--- app/src/main/java/com/example/myapplication/Vista2.java
+++ app/src/main/java/com/example/myapplication/Vista2.java
@@ -2,16 +2,45 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class Vista2 extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista2);
 
+        listView = (ListView) findViewById(R.id.listview);
+
+        names = new ArrayList<String>();
+        names.add("Veracruz");
+        names.add("Tabasco");
+        names.add("Chiapas");
+        names.add("Campeche");
+        names.add("Quintana Roo");
+        names.add("Jalisco");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,names);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Vista2.this, "Picaste " + names.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
